3d8faf63e258daff65af04e88640b86d3a4c15f9
Dara Oladosu <daradosu@gmail.com>
2019-09-01 01:06:22 +0100

    stub out vuex stores

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+import store from './store'
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
@@ -12,6 +13,7 @@ Vue.use(Vuetify)
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>',
   vuetify: new Vuetify({})
